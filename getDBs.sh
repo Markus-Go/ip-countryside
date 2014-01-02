e5e6f0d9442ce6490f89d309d55ce0e0613c0018
Markus Goldstein <Markus-Go@users.noreply.github.com>
2014-01-02 14:03:56 +0000

    Updating getDBs script.

--- getDBs.sh
+++ getDBs.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
-rm -f delegated-afrinic-latest
-rm -f delegated-lacnic-latest
-rm -f delegated-arin-latest
-rm -f delegated-apnic-latest
-rm -f delegated-ripencc-latest
-rm -f ripe.db.inetnum
-rm -f apnic.db.inetnum
-rm -f ripe.db.inetnum.gz
-rm -f apnic.db.inetnum.gz
-wget ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest
-wget ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest
-wget ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest 
-wget ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest
-wget ftp://ftp.ripe.net/ripe/stats/delegated-ripencc-latest
-wget ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz
+WGET='/usr/bin/wget'
+MD5SUM='/usr/bin/md5sum'
+GUNZIP='/bin/gunzip'
+RM='/bin/rm'
+
+$RM -r delegated-afrinic-latest.md5
+$WGET ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest.md5
+if ! $MD5SUM --check delegated-afrinic-latest.md5 > /dev/null 2>&1; then
+  $RM -f delegated-afrinic-latest
+  $WGET ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest
+fi
+
+$RM -f delegated-lacnic-latest.md5
+$WGET ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest.md5
+if ! $MD5SUM --check delegated-lacnic-latest.md5 > /dev/null 2>&1; then
+  $RM -f delegated-lacnic-latest
+  $WGET ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest
+fi
+
+$RM -f delegated-arin-extended-latest.md5
+$WGET ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest.md5
+sed -i 's/[0-9]*$/latest/g' delegated-arin-extended-latest.md5
+if ! $MD5SUM --check delegated-arin-extended-latest.md5 > /dev/null 2>&1; then
+  $RM -f delegated-arin-extended-latest
+  $WGET ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
+fi
+
+$RM -f delegated-apnic-latest.md5
+$WGET ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest.md5
+if ! $MD5SUM --check delegated-apnic-latest.md5 > /dev/null 2>&1; then
+  $RM -f delegated-apnic-latest
+  $RM -f apnic.db.inetnum*
+  $WGET ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest
 ftp -n -v ftp.apnic.net <<END
-user anonymous anonymous@anonymous.org
-binary
-passive
-get /apnic/whois-data/APNIC/split/apnic.db.inetnum.gz apnic.db.inetnum.gz
-bye
+  user anonymous -anonymous@anonymous.org
+  binary
+  passive
+  get /apnic/whois-data/APNIC/split/apnic.db.inetnum.gz apnic.db.inetnum.gz
+  bye
 END
-gunzip ripe.db.inetnum
-gunzip apnic.db.inetnum
+  $GUNZIP apnic.db.inetnum
+fi
+
+$RM -f delegated-ripencc-latest.md5
+$WGET ftp://ftp.ripe.net/ripe/stats/delegated-ripencc-latest.md5
+if ! $MD5SUM --check delegated-ripencc-latest.md5 > /dev/null 2>&1; then
+  $RM -f delegated-ripencc-latest
+  $RM -f ripe.db.inetnum*
+  $WGET ftp://ftp.ripe.net/ripe/stats/delegated-ripencc-latest
+  $WGET ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz
+  $GUNZIP ripe.db.inetnum
+fi
