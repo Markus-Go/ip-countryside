8fca95eef84e5d3ed26a05bac4f55d8b80b5fd65
mjarkas97 <88595848+mjarkas97@users.noreply.github.com>
2022-03-23 14:56:39 +0100

    BugFixes

--- del_files_parser.py
+++ del_files_parser.py
@@ -87,15 +87,23 @@ def strip_del_files(del_destination_stripped_name = "del_merged_stripped.txt"):
 
     # Removes all IPv6 and other lines 
     for line in fileinput.input(os.path.join("del_files", "del_merged.txt"), inplace = True):
+        
         if re.search(ipv4_pattern, line):
+            
             list = line.split("|")
+            
+            if list[6] == 'reserved':
+                
+                list[1] = "N/A"
+                
             cidr = int(getSubnetmask(list[4]))
             addr = [int(x) for x in list[3].split('.')]
             mask = [( ((1<<32)-1) << (32-cidr) >> i ) & 255 for i in reversed(range(0, 32, 8))]
             netw = [addr[i] & mask[i] for i in range(4)]
             bcas = [(addr[i] & mask[i]) | (255^mask[i]) for i in range(4)]
             
-            ranges = ipv4_to_int(netw, bcas)
+            ranges = [ipv4_to_int(netw), ipv4_to_int(bcas)]
+            
             ranges = map(str, ranges) 
             ranges = ','.join(ranges)
             
@@ -103,36 +111,37 @@ def strip_del_files(del_destination_stripped_name = "del_merged_stripped.txt"):
 
             print(*list, end='\n')
 
-def ipv4_to_int(minIP, maxIP):
+def ipv4_to_int(ip):
 
-    intIPMin = (minIP[0] * 16777216) + (minIP[1] * 65536) + (minIP[2] * 256 ) + (minIP[3] * 1)
-    intIPMax = (maxIP[0] * 16777216) + (maxIP[1] * 65536) + (maxIP[2] * 256 ) + (maxIP[3] * 1)
+    return (ip[0] * 16777216) + (ip[1] * 65536) + (ip[2] * 256 ) + (ip[3] * 1)
 
-    return [intIPMin, intIPMax]
+def int_to_ipv4(ipInt):
+    return [(ipInt / 16777216) , (ipInt / 65536) , (ipInt / 256 ) , (ipInt / 1)]
 
 def getCountryCode(ip):
+   
+    ip = ip.split('.')
+    ip = [int(x) for x in ip]
+    ip = ipv4_to_int(ip)
 
     with open(os.path.join("del_files", "del_merged.txt")) as file:
         
         for line in file: 
-           
+
            item = line.split(" ")
 
            [ranges, country] = item 
 
            [minIP, maxIP] = ranges.split(",")
-           
-           ip = [int(x) for x in ip.split('.')]
-
-           ip = (ip[0] * 16777216) + (ip[1] * 65536) + (ip[2] * 256 ) + (ip[3] * 1)
-
 
            if ipv4_in_range(int(minIP), int(maxIP), ip):
+                print(line)
                 return country
 
-           return 'No Country Found!' 
 
-           
+        return 'No Country Found!' 
+
+
 def ipv4_in_range(min, max, ip):
     return min <= ip <= max
 
@@ -150,8 +159,8 @@ def getSubnetmask(hosts):
 
     return "0"
 
-#merge_del_files()    # Fügt del Dateien in del_merged.txt zusammen
-#strip_del_files()    # formatiert und filtert Textdatei
+merge_del_files()    # Fügt del Dateien in del_merged.txt zusammen
+strip_del_files()    # formatiert und filtert Textdatei
 
 # print(getCountryCode("30.142.194.176"))     # Fehler beim "berechnen" der Subnetzmaske
 
@@ -160,9 +169,7 @@ def getSubnetmask(hosts):
 #print("192.212.211.222" + "/" + str(getSubnetmask(1024)))          # manuell die Zahl eingeben funktioniert ohne Probleme 
 
 
-#print(getCountryCode("94.134.100.189"))
+# ip = [int(x) for x in "94.134.100.189".split('.')]
+# print(ipv4_to_int(ip))
 
-ip = [94, 134, 100, 189]
-ip = (ip[0] * 16777216) + (ip[1] * 65536) + (ip[2] * 256 ) + (ip[3] * 1)
-
-print(ip)
+print(getCountryCode("94.134.100.189"))   
