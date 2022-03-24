5f6f44ab94e7dd9d0ca38670d6dc4ac2ff4b0c13
malldavid231 <91737102+malldavid231@users.noreply.github.com>
2022-03-24 13:49:09 +0100

    Added IPv6 support

--- del_files_parser.py
+++ del_files_parser.py
@@ -52,41 +52,46 @@ def merge_del_files(merged_del_file = "merged_del_files.txt"):
 # @TODO define stipped_del_file as global name 
 def strip_del_files(stipped_del_file = "stipped_del_files.txt"):
 
-    ipv4_pattern = "[0-9]+(?:\.[0-9]+){3}"
-    # @TODO ipv6 ...
-    ipv6_pattern = ""
+    with open(os.path.join(DEL_FILES_DIR, "ipv4_del.txt"), 'w') as ip4, open(os.path.join(DEL_FILES_DIR, "ipv6_del.txt"), 'w') as ip6:
+        for fileline in fileinput.input(os.path.join(DEL_FILES_DIR, "merged_del_files.txt")):
 
-    # Removes all IPv6 and other lines 
-    for line in fileinput.input(os.path.join(DEL_FILES_DIR, "merged_del_files.txt"), inplace = True):
-        
-        if re.search(ipv4_pattern, line):
-            
-            list = parse_ipv4(line)
- 
-            print(*list, end='\n')
+            if re.search(IPV4_PATTERN, fileline):
 
-        if re.search(ipv6_pattern, line):
+                fileline = parse_ipv4(fileline)
+                fileline = " ".join(map(str, fileline))
+                ip4.write(str(fileline)+'\n')
 
-            # @TODO IPv6 ... Do Something here ...
-            list = parse_ipv6(line)
+            elif re.search(IPV6_PATTERN, fileline):
 
-            # print(*list, end='\n')
+                fileline = parse_ipv6(fileline)
+                fileline = " ".join(map(str, fileline))
+                ip6.write(str(fileline)+'\n')
 
 
-def parse_ipv4(line):
+
+def parse_ipv6(line):
+
+    record = line.split("|")
+    if record[6] == 'reserved':
+        record[1] = "N/A"
+
+    return [record[3], record[4], record[1]]
+
+def parse_ipv4(line):           # Diese Klasse verwerfen und die ipaddr Klasse verwenden?
+                                # @TODO Muss noch verändert werden dass nur ip + cidr + country code in textdatei steht
 
     # registry|cc|type|start|value|date|status[|extensions...]
 
     record = line.split("|")
 
-    # if line doesn't have any country 
+    # if line doesn't have any country
     if record[6] == 'reserved':
         record[1] = "N/A"
 
     cidr = int(get_subnet_mask(record[4]))
     
     addr = [int(x) for x in record[3].split('.')]
-    
+
     mask = [( ((1<<32)-1) << (32-cidr) >> i ) & 255 for i in reversed(range(0, 32, 8))]
     
     netw = [addr[i] & mask[i] for i in range(4)]
@@ -99,7 +104,7 @@ def parse_ipv4(line):
     
     ranges = ','.join(ranges)
     
-    record = ranges, record[1]
+    record = [ranges, record[1]]
 
     return record
 
@@ -113,35 +118,51 @@ def ipv4_in_range(min, max, ip):
     return min <= ip <= max
 
 
-def parse_ipv6(line):
-    return ""
+def ip_in_range(ip, network, mask):
+    return ipaddress.ip_address(ip) in ipaddress.ip_network(network + "/" + mask)
 
 
 def get_country_code(ip):
-   
-    ip = ip.split('.')
-    ip = [int(x) for x in ip]
-    ip = ipv4_to_int(ip)
 
-    with open(os.path.join(DEL_FILES_DIR, "merged_del_files.txt")) as file:
-        
-        for line in file: 
+    expr = re.match(IPV4_PATTERN, ip)
 
-           item = line.split(" ")
+    if expr:
+        ip = ip.split('.')              # 3 Zeilen brauchen wir nicht mehr?
+        ip = [int(x) for x in ip]
+        ip = ipv4_to_int(ip)
+        with open(os.path.join(DEL_FILES_DIR, "ipv4_del.txt")) as file:
 
-           [ranges, country] = item 
+            for line in file:
 
-           [minIP, maxIP] = ranges.split(",")
+                item = line.split("|")
 
-           if ipv4_in_range(int(minIP), int(maxIP), ip):
-                country = country.rstrip('\n')
-                return COUNTRY_DICTIONARY[country], country
+                network = item[3]
+                nr_max_hosts = item[4]
+                mask = get_subnet_mask(nr_max_hosts)
+
+                if ip_in_range(ip, network, mask):
+                    country = country.rstrip('\n')
+                    return COUNTRY_DICTIONARY[country], country
+        return 'No Country Found!'
+    else:
+        with open(os.path.join(DEL_FILES_DIR, "ipv6_del.txt")) as file:
+
+            for line in file:
+
+                item = line.split()
+
+                network = item[0]
+                mask = item[1]
+
+                if ip_in_range(ip, network, mask):
+                    country = item[2].rstrip('\n')
+                    return COUNTRY_DICTIONARY[country], country
+        return 'No Country Found!'
 
 
-    return 'No Country Found!' 
 
       
-def get_subnet_mask(hosts):
+def get_subnet_mask(hosts):     # @TODO Nicht immer aufrunden?
     
     hosts = math.log2(int(hosts))      
     hosts_aufg = math.ceil(hosts)     
@@ -157,7 +178,16 @@ def get_subnet_mask(hosts):
 
 def main():
 
-    merge_del_files()    # Fügt del Dateien in del_merged.txt zusammen
-    strip_del_files()    # formatiert und filtert Textdatei
-    
+    #merge_del_files()    # Fügt del Dateien in del_merged.txt zusammen
+    #strip_del_files()    # formatiert und filtert Textdatei
+    #print(int(ipaddress.ip_address("2001::")))
+
+    #print(ipaddress.ip_address('2001:0db8:1234::') in ipaddress.ip_network('2001:0db8::/32'))
+    #print(('691634176,691650559', 'ZA').join)
+
+    print(get_country_code("2001:43f8:1d0::"))
+
+
 main()
+
+
