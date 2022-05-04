cf494e6f5620134894432d618e261f4c50137549
CSharper96 <thomashorny@gmx.net>
2022-05-04 13:18:34 +0200

    Update ip_countryside_cmi.py

--- Utilities/ip_countryside_cmi.py
+++ Utilities/ip_countryside_cmi.py
@@ -1,6 +1,8 @@
 #Comand line interpreter
 
 import click
+import ipaddress
+import sys
 
 @click.group()
 def cli():
@@ -9,16 +11,45 @@ def cli():
 
 @cli.command(name='update', help='update database')
 @click.option('--force', '-f', is_flag=True, help="Forcing update.")
-def update(force):
-    if force:
-        click.echo('forcing update')
-    else:
-        click.echo('updating if old enough')
+@click.option('--output', '-o', multiple=True, help="Format option for update.")
+@click.option('--split', '-s', is_flag=True, help="Seperating IPv4 and IPv6.")
+def update(force, output, split):
+    #if force:
+    #    click.echo('forcing update\n' + 'output = ' + output + '\nSeperating IP = ' + ip)
+    #else:
+    #    click.echo('updating if old enough\n' + 'output = ' + join(output) + '\nSeperating IP = ' + ip)
+    click.echo('Is Force = ' + str(force))
+    click.echo('\nOutput options:')
+    click.echo('\n'.join(output))
+    click.echo('\nSplit IP = ' + str(split))
 
-@cli.command(name='parse', help='parsing IP')
+@cli.command(name='trace', help='tracing IP')
 @click.argument('IP')
-def parse(ip):
-    click.echo('Parsing ' + ip)
+def trace(ip):
+    if(checkIp(ip)):
+        click.echo('tracing ' + ip)
+    else:
+        pass
+
+@cli.command(name='query', help='querying IP')
+@click.argument('IP')
+def query(ip):
+    if(checkIp(ip)):
+        click.echo('querying ' + ip)
+    else:
+        pass
+
+def checkIp(ip):
+    try:
+        ip = ipaddress.ip_address(ip)
+        click.echo('%s is a correct IP%s address.' % (ip, ip.version))
+        return True
+    except ValueError:
+        click.echo('address/netmask is invalid: %s' % sys.argv[1])
+        return False
+    except:
+        click.echo('Usage : %s  ip' % sys.argv[0])
+        return False
 
 if __name__ == "__main__":
     cli()
