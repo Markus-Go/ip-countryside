acbbb9fb19f6a84c390503891c64d1db029477c4
mjarkas97 <88595848+mjarkas97@users.noreply.github.com>
2022-05-07 15:54:55 +0200

    WIP

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -15,3 +15,25 @@ function init_map(map, lat, lon, ip) {
     .openPopup();
 
 }
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'text-success';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'text-primary';
+            } else {
+                cls = 'text-info';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'text-danger';
+        } else if (/null/.test(match)) {
+            cls = 'text-secondary';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+        })
+}
--- flaskr/static/svelte/components/API.svelte
+++ flaskr/static/svelte/components/API.svelte
@@ -22,27 +22,6 @@ async function handle_search_by_api() {
     ip = "" 
 }
 
-function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
-    }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'text-success';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'text-primary';
-            } else {
-                cls = 'text-info';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'text-danger';
-        } else if (/null/.test(match)) {
-            cls = 'text-secondary';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-        })
-}
 
 </script>
     
